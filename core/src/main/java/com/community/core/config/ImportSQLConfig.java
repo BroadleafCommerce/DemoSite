638e358658206dcb9419161f560eb09a307132fb
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2017-03-31 13:26:39 -0500

    Removed ImportSQLConfig since there's no custom sql that needs to be ran

--- core/src/main/java/com/community/core/config/ImportSQLConfig.java
+++
@@ -1,9 +0,0 @@
-package com.community.core.config;
-
-import org.broadleafcommerce.common.demo.ImportCondition;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration("blCommunityDemoData")
-@Conditional(ImportCondition.class)
-public class ImportSQLConfig {}
