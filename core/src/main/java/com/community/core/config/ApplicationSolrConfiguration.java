4532fe14f1a063ac5b98f28bf993753f285b830e
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2017-03-31 19:03:09 -0500

    Removed OMS solr config since OMS isn't included

--- core/src/main/java/com/community/core/config/ApplicationSolrConfiguration.java
+++ core/src/main/java/com/community/core/config/ApplicationSolrConfiguration.java
@@ -26,33 +26,6 @@ public class ApplicationSolrConfiguration {
     @Value("${solr.url.admin}")
     protected String adminCatalogSolrUrl;
 
-    @Value("${oms.search.order.service.solr.primary.location}")
-    protected String primaryOrderSolrUrl;
-
-    @Value("${oms.search.order.service.solr.reindex.location}")
-    protected String reindexOrderSolrUrl;
-
-    @Value("${oms.search.order.service.solr.admin.location}")
-    protected String adminOrderSolrUrl;
-
-    @Value("${oms.search.customer.service.solr.primary.location}")
-    protected String primaryCustomerSolrUrl;
-
-    @Value("${oms.search.customer.service.solr.reindex.location}")
-    protected String reindexCustomerSolrUrl;
-
-    @Value("${oms.search.customer.service.solr.admin.location}")
-    protected String adminCustomerSolrUrl;
-
-    @Value("${oms.search.fulfillmentOrder.service.solr.primary.location}")
-    protected String primaryFulfillmentOrderSolrUrl;
-
-    @Value("${oms.search.fulfillmentOrder.service.solr.reindex.location}")
-    protected String reindexFulfillmentOrderSolrUrl;
-
-    @Value("${oms.search.fulfillmentOrder.service.solr.admin.location}")
-    protected String adminFulfillmentOrderSolrUrl;
-    
     @Bean
     public SolrClient primaryCatalogSolrClient() {
         return new HttpSolrClient(primaryCatalogSolrUrl);
@@ -69,70 +42,10 @@ public class ApplicationSolrConfiguration {
     }
 
     @Bean
-    public SolrClient primaryOrderSolrClient() {
-        return new HttpSolrClient(primaryOrderSolrUrl);
-    }
-
-    @Bean
-    public SolrClient reindexOrderSolrClient() {
-        return new HttpSolrClient(reindexOrderSolrUrl);
-    }
-
-    @Bean
-    public SolrClient adminOrderSolrClient() {
-        return new HttpSolrClient(adminOrderSolrUrl);
-    }
-
-    @Bean
-    public SolrClient primaryCustomerSolrClient() {
-        return new HttpSolrClient(primaryCustomerSolrUrl);
-    }
-
-    @Bean
-    public SolrClient reindexCustomerSolrClient() {
-        return new HttpSolrClient(reindexCustomerSolrUrl);
-    }
-
-    @Bean
-    public SolrClient adminCustomerSolrClient() {
-        return new HttpSolrClient(adminCustomerSolrUrl);
-    }
-
-    @Bean
-    public SolrClient primaryFulfillmentOrderSolrClient() {
-        return new HttpSolrClient(primaryFulfillmentOrderSolrUrl);
-    }
-
-    @Bean
-    public SolrClient reindexFulfillmentOrderSolrClient() {
-        return new HttpSolrClient(reindexFulfillmentOrderSolrUrl);
-    }
-
-    @Bean
-    public SolrClient adminFulfillmentOrderSolrClient() {
-        return new HttpSolrClient(adminFulfillmentOrderSolrUrl);
-    }
-
-    @Bean
     public SolrConfiguration blCatalogSolrConfiguration() throws IllegalStateException {
         return new SolrConfiguration(primaryCatalogSolrClient(), reindexCatalogSolrClient(), adminCatalogSolrClient());
     }
 
-    @Bean
-    public SolrConfiguration blOrderSolrConfiguration() throws IllegalStateException {
-        return new SolrConfiguration(primaryOrderSolrClient(), reindexOrderSolrClient(), adminOrderSolrClient());
-    }
-
-    @Bean
-    public SolrConfiguration blCustomerSolrConfiguration() throws IllegalStateException {
-        return new SolrConfiguration(primaryCustomerSolrClient(), reindexCustomerSolrClient(), adminCustomerSolrClient());
-    }
-
-    @Bean
-    public SolrConfiguration blFulfillmentOrderSolrConfiguration() throws IllegalStateException {
-        return new SolrConfiguration(primaryFulfillmentOrderSolrClient(), reindexFulfillmentOrderSolrClient(), adminFulfillmentOrderSolrClient());
-    }
-    
     @Bean
     protected SearchService blSearchService() {
         return new SolrSearchServiceImpl();
