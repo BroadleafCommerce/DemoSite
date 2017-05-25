d7d0ea98b3b72cc764b05dbf650aaf16592eed42
Philip Baggett <pbaggett@broadleafcommerce.com>
2017-05-25 16:53:17 -0500

    #8 - Removing endpoints that now have framework mapping annotations.

--- api/src/main/java/com/community/api/endpoint/cart/CartEndpoint.java
+++ api/src/main/java/com/community/api/endpoint/cart/CartEndpoint.java
@@ -17,19 +17,11 @@
 package com.community.api.endpoint.cart;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.broadleafcommerce.rest.api.wrapper.OrderAttributeWrapper;
-import com.broadleafcommerce.rest.api.wrapper.OrderItemAttributeWrapper;
-import com.broadleafcommerce.rest.api.wrapper.OrderItemWrapper;
 import com.broadleafcommerce.rest.api.wrapper.OrderWrapper;
-
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -47,7 +39,6 @@ import javax.servlet.http.HttpServletRequest;
                 produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 public class CartEndpoint extends com.broadleafcommerce.rest.api.endpoint.order.CartEndpoint {
 
-    
     @Override
     @RequestMapping(value = "", method = RequestMethod.GET)
     public OrderWrapper findCartForCustomer(HttpServletRequest request) {
@@ -57,113 +48,5 @@ public class CartEndpoint extends com.broadleafcommerce.rest.api.endpoint.order.
             // if we failed to find the cart, create a new one
             return createNewCartForCustomer(request);
         }
-    }
-
-    @Override
-    @RequestMapping(value = "", method = RequestMethod.POST)
-    public OrderWrapper createNewCartForCustomer(HttpServletRequest request) {
-        return super.createNewCartForCustomer(request);
-    }
-
-    @Override
-    @RequestMapping(value = "/{cartId}", method = RequestMethod.GET)
-    public OrderWrapper findCartById(HttpServletRequest request,
-                                     @PathVariable("cartId") Long cartId) {
-        return super.findCartById(request, cartId);
-    }
-
-    @RequestMapping(value = "/{cartId}/item", method = RequestMethod.POST)
-    public OrderWrapper addItemToOrder(HttpServletRequest request,
-                                          @PathVariable("cartId") Long cartId,
-                                          @RequestParam(value = "priceOrder", required = false, defaultValue = "true") Boolean priceOrder,
-                                          @RequestBody OrderItemWrapper orderItemWrapper) {
-        return super.addItemToOrder(request, cartId, orderItemWrapper, priceOrder);
-    }
-
-    @Override
-    @RequestMapping(value = "/{cartId}/items/{itemId}", method = RequestMethod.DELETE)
-    public OrderWrapper removeItemFromOrder(HttpServletRequest request,
-                                            @PathVariable("itemId") Long itemId,
-                                            @PathVariable("cartId") Long cartId,
-                                            @RequestParam(value = "priceOrder", required = false, defaultValue = "true") Boolean priceOrder) {
-        return super.removeItemFromOrder(request, itemId, cartId, priceOrder);
-    }
-
-    @Override
-    @RequestMapping(value = "/{cartId}/items/{itemId}", method = RequestMethod.PUT)
-    public OrderWrapper updateItemQuantity(HttpServletRequest request,
-                                           @PathVariable("itemId") Long itemId,
-                                           @PathVariable("cartId") Long cartId,
-                                           @RequestParam("quantity") Integer quantity,
-                                           @RequestParam(value = "priceOrder", required = false, defaultValue = "true") Boolean priceOrder) {
-        return super.updateItemQuantity(request, itemId, cartId, quantity, priceOrder);
-    }
-
-    @Override
-    @RequestMapping(value = "/{cartId}/offer/{promoCode}", method = RequestMethod.POST)
-    public OrderWrapper addOfferCode(HttpServletRequest request,
-                                     @PathVariable("promoCode") String promoCode,
-                                     @PathVariable("cartId") Long cartId,
-                                     @RequestParam(value = "priceOrder", required = false, defaultValue = "true") Boolean priceOrder) {
-        return super.addOfferCode(request, promoCode, cartId, priceOrder);
-    }
-
-    @Override
-    @RequestMapping(value = "/{cartId}/offer/{promoCode}", method = RequestMethod.DELETE)
-    public OrderWrapper removeOfferCode(HttpServletRequest request,
-                                        @PathVariable("promoCode") String promoCode,
-                                        @PathVariable("cartId") Long cartId,
-                                        @RequestParam(value = "priceOrder", required = false, defaultValue = "true") Boolean priceOrder) {
-        return super.removeOfferCode(request, promoCode, cartId, priceOrder);
-    }
-
-    @Override
-    @RequestMapping(value = "/{cartId}/offers", method = RequestMethod.DELETE)
-    public OrderWrapper removeAllOfferCodes(HttpServletRequest request,
-                                            @PathVariable("cartId") Long cartId,
-                                            @RequestParam(value = "priceOrder", required = false, defaultValue = "true") boolean priceOrder) {
-        return super.removeAllOfferCodes(request, cartId, priceOrder);
-    }
-
-    @Override
-    @RequestMapping(value = "/{cartId}/attributes", method = RequestMethod.PUT,
-        consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    public OrderWrapper updateOrderAttributes(HttpServletRequest request,
-                                              @RequestBody List<OrderAttributeWrapper> attributes,
-                                              @PathVariable("cartId") Long cartId,
-                                              @RequestParam(value = "priceOrder", required = false, defaultValue = "true") Boolean priceOrder) {
-        return super.updateOrderAttributes(request, attributes, cartId, priceOrder);
-    }
-
-    @Override
-    @RequestMapping(value = "/{cartId}/attributes", method = RequestMethod.DELETE,
-        consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    public OrderWrapper deleteOrderAttributes(HttpServletRequest request,
-                                              @RequestBody List<OrderAttributeWrapper> requestParams,
-                                              @PathVariable("cartId") Long cartId,
-                                              @RequestParam(value = "priceOrder", required = false, defaultValue = "true") Boolean priceOrder) {
-        return super.deleteOrderAttributes(request, requestParams, cartId, priceOrder);
-    }
-
-    @Override
-    @RequestMapping(value = "/{cartId}/item/{itemId}/attributes", method = RequestMethod.PUT,
-        consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    public OrderWrapper updateProductOptions(HttpServletRequest request,
-                                             @RequestBody List<OrderItemAttributeWrapper> requestParams,
-                                             @PathVariable("cartId") Long cartId,
-                                             @PathVariable("itemId") Long itemId,
-                                             @RequestParam(value = "priceOrder", required = false, defaultValue = "true") Boolean priceOrder) {
-        return super.updateProductOptions(request, requestParams, cartId, itemId, priceOrder);
-    }
-
-    @Override
-    @RequestMapping(value = "/{cartId}/item/{itemId}/attributes", method = RequestMethod.DELETE,
-        consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    public OrderWrapper deleteProductOptions(HttpServletRequest request,
-                                             @RequestBody(required = false) List<OrderItemAttributeWrapper> requestParams,
-                                             @PathVariable("cartId") Long cartId,
-                                             @PathVariable("itemId") Long itemId,
-                                             @RequestParam(value = "priceOrder", required = false, defaultValue = "true") Boolean priceOrder) {
-        return super.deleteProductOptions(request, requestParams, cartId, itemId, priceOrder);
     }
 }
