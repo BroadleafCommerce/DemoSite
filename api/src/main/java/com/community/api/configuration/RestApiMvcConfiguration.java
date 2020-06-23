7632a3a1941361564888dac7716ba82fe818ddc3
oleksiimiroshnyk <omiroshnyk@broadleafcommerce.com>
2020-06-24 00:41:09 +0300

    Migrate from Swagger 2 to OpenAPI 3 for BLC 6.1 (#67)

--- api/src/main/java/com/community/api/configuration/RestApiMvcConfiguration.java
+++ api/src/main/java/com/community/api/configuration/RestApiMvcConfiguration.java
@@ -1,41 +1,18 @@
 package com.community.api.configuration;
 
+import com.broadleafcommerce.rest.api.BroadleafRestApiMvcConfiguration;
 import org.broadleafcommerce.common.web.controller.annotation.EnableFrameworkRestControllers;
 import org.broadleafcommerce.common.web.filter.FilterOrdered;
 import org.broadleafcommerce.common.web.filter.IgnorableOpenEntityManagerInViewFilter;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springdoc.core.SpringDocConfigProperties;
+import org.springdoc.core.SpringDocConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-
-import com.broadleafcommerce.rest.api.BroadleafRestApiMvcConfiguration;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.BasicAuth;
-import springfox.documentation.service.ResponseMessage;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.OperationBuilderPlugin;
-import springfox.documentation.spi.service.contexts.OperationContext;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Configuration
@@ -86,78 +63,13 @@ public class RestApiMvcConfiguration extends BroadleafRestApiMvcConfiguration {
         registry.addResourceHandler("/images/favicon-16x16.png")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/images/favicon-16x16.png");
     }
-    
-    @EnableSwagger2
-    @Configuration
-    public static class SwaggerConfig {
-    
-        @Bean
-        public Docket globalApi() {
-            return new Docket(DocumentationType.SWAGGER_2)
-                    .select()
-                      .apis(RequestHandlerSelectors.any())
-                      .paths(PathSelectors.any())
-                      .build()
-                    .securitySchemes(Arrays.asList(new BasicAuth("basicAuth")))
-                    .securityContexts(Arrays.asList(securityContext()))
-                    .useDefaultResponseMessages(false)
-                    .apiInfo(apiInfo());
-        }
-        
-        private SecurityContext securityContext() {
-            return SecurityContext.builder()
-                .securityReferences(basicAuth())
-                .forPaths(PathSelectors.any())
-                .build();
-        }
-    
-        private List<SecurityReference> basicAuth() {
-            return Arrays.asList(SecurityReference.builder()
-                .reference("basicAuth")
-                .scopes(new AuthorizationScope[0])
-                .build());
-        }
-    
-    
-        protected ApiInfo apiInfo () {
-            return new ApiInfoBuilder().title("Broadleaf Commerce API")
-                    .description("The default Broadleaf Commerce APIs")
-                    .version("v1")
-                    .build();
-        }
-    
-        @Bean
-        public TranslationOperationBuilderPlugin translationPlugin() {
-            return new TranslationOperationBuilderPlugin();
-        }
-        
-        @Order(Ordered.LOWEST_PRECEDENCE)
-        public static class TranslationOperationBuilderPlugin implements OperationBuilderPlugin {
-    
-            @Autowired
-            protected MessageSource translator;
-            
-            @Override
-            public boolean supports(DocumentationType delimiter) {
-                return true;
-            }
-    
-            @Override
-            public void apply(OperationContext context) {
-                Set<ResponseMessage> messages = context.operationBuilder().build().getResponseMessages();
-                Set<ResponseMessage> translated = new HashSet<>();
-                for (ResponseMessage untranslated : messages) {
-                    String translation = translator.getMessage(untranslated.getMessage(), null, untranslated.getMessage(), null);
-                    translated.add(new ResponseMessage(untranslated.getCode(),
-                        translation,
-                        untranslated.getResponseModel(),
-                        untranslated.getHeaders(),
-                        untranslated.getVendorExtensions()));
-                }
-                context.operationBuilder().responseMessages(translated);
-            }
-            
-        }
-    }
 
+    @Bean
+    SpringDocConfiguration springDocConfiguration(){
+        return new SpringDocConfiguration();
+    }
+    @Bean
+    public SpringDocConfigProperties springDocConfigProperties() {
+        return new SpringDocConfigProperties();
+    }
 }
